108aaf28dc31d77fba86bcf6c2fb7b205eb0224a
youssefhammad <youssefhammad977@gmail.com>
2023-07-30 02:41:11 +0300

    Implement & validate addImage controller

--- NominalBackend/Domain/Images/Services/ImageService.cs
+++ NominalBackend/Domain/Images/Services/ImageService.cs
@@ -6,12 +6,21 @@ namespace NominalBackend.Domain.Images.Services
 {
     public interface IImageService : ICrudService<Image>
     {
-
+        public Task<bool> IsAPhotoFile(string fileName);
     }
     public class ImageService : CrudService<Image>, IImageService
     {
         public ImageService(IUnitOfWork unitOfWork, ICrudRepository<Image> repository) : base(unitOfWork, repository)
         {
+        }
+
+        public async Task<bool> IsAPhotoFile(string fileName)
+        {
+            return fileName.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase)
+                || fileName.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase)
+                || fileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase)
+                || fileName.EndsWith(".jfif", StringComparison.OrdinalIgnoreCase)
+                || fileName.EndsWith(".pjp", StringComparison.OrdinalIgnoreCase);
         }
     }
 }
