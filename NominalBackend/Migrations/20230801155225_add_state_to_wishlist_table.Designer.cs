4c71b95568dfe60b98484896f23f811852351c8c
youssefhammad <youssefhammad977@gmail.com>
2023-08-01 18:54:55 +0300

    add state to wishlisttable //need update-database

--- NominalBackend/Migrations/20230801155225_add_state_to_wishlist_table.Designer.cs
+++ NominalBackend/Migrations/20230801155225_add_state_to_wishlist_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NominalBackend.Persistence;
 
@@ -11,9 +12,11 @@ using NominalBackend.Persistence;
 namespace NominalBackend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230801155225_add_state_to_wishlist_table")]
+    partial class add_state_to_wishlist_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
