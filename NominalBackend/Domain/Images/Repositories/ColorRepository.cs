3b3d2da313894abf5e6daf7befbf30e732ed5899
Hesham236 <mohamed.hesham9780@gmail.com>
2023-08-03 02:02:15 +0300

    implement crud for color domain

---
+++ NominalBackend/Domain/Images/Repositories/ColorRepository.cs
@@ -0,0 +1,17 @@
+﻿using NominalBackend.Domain.Images.Models;
+using NominalBackend.Generics;
+using NominalBackend.Persistence;
+
+namespace NominalBackend.Domain.Images.Repositories
+{
+    public interface IColorRepository : ICrudRepository<Color>
+    {
+
+    }
+    public class ColorRepository : CrudRepository<Color>, IColorRepository
+    {
+        public ColorRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
